c1c8be7bcf148d1db73ff4a7b340759582643200
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-04-10 18:40:13 +0100

    refactor database configuration to use environment variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,16 @@
+// This file contains the database configuration for different environments
+// such as development, test, and production.
+
+const dotenv = require("dotenv");
+dotenv.config();
+
 module.exports = {
   development: {
-    username: "root",
-    password: null,
-    database: "database_development",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.DB_DEV_USER,
+    password: process.env.DB_DEV_PASSWORD,
+    database: process.env.DB_DEV_NAME,
+    host: process.env.DB_DEV_HOST,
+    dialect: process.env.DB_DEV_DIALECT,
   },
   test: {
     username: "root",
