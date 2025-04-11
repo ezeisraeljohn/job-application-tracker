370b5f27076e3a2587a9215cc5f6bf69daa3ceb5
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-04-11 14:26:30 +0100

    Add job and resume routes, update health check endpoints in app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const resumeRoutes = require("./routes/resume.routes");
+const jobRoutes = require("./routes/job.routes");
 const authRoutes = require("./routes/auth.routes");
 const express = require("express");
 const cors = require("cors");
@@ -8,9 +10,16 @@ app.use(cors());
 app.use(express.json());
 
 // Health Check
-app.get("/", (req, res) => res.json("App is Running"));
+app.get("/", (req, res) => res.json("Job Tracker API"));
+app.get("/api/v1", (req, res) => res.json("Job Tracker API v1"));
+app.get("/api/v1/health", (req, res) => res.json("Job Tracker API is healthy"));
+app.get("/api/v1/status", (req, res) => res.json("Job Tracker API is running"));
 
-//Auth Routes
+//Auth Route
 app.use("/api/v1/auth", authRoutes);
+
+// Other Routes
+app.use("/api/v1/jobs", jobRoutes);
+app.use("/api/v1/resume", resumeRoutes);
 
 module.exports = app;
