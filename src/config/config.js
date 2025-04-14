a9e9c8fe0a24cb850b690bcc50993ba8ece212f8
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-04-14 16:33:54 +0100

    config dialects for dev and test changed to postgres

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,8 @@
 // such as development, test, and production.
 
 const dotenv = require("dotenv");
+const fs = require("fs");
+const path = require("path");
 dotenv.config();
 
 module.exports = {
@@ -10,20 +12,31 @@ module.exports = {
     password: process.env.DB_DEV_PASSWORD,
     database: process.env.DB_DEV_NAME,
     host: process.env.DB_DEV_HOST,
-    dialect: process.env.DB_DEV_DIALECT,
+    dialect: "postgres",
   },
   test: {
     username: "root",
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: "postgres",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    use_env_variable: "DB_PROD_URL",
+    username: process.env.DB_PROD_USER,
+    password: process.env.DB_PROD_PASSWORD,
+    database: process.env.DB_PROD_NAME,
+    host: process.env.DB_PROD_HOST,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        ca: fs
+          .readFileSync(path.resolve(__dirname, "../cert/ca.pem"))
+          .toString(),
+        rejectUnauthorized: false, // Set this true for Aiven's signed cert
+      },
+    },
+    logging: false,
   },
 };
