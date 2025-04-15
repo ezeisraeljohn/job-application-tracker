9906ad77677161bc6b4b7bf0159f21b1f7a0f727
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-04-15 01:55:43 +0100

    Refactor axios instance to remove dotenv configuration and simplify base URL setup

--- client/services/api.ts
+++ client/services/api.ts
@@ -1,13 +1,9 @@
 import axios from "axios";
-import dotenv, { config } from "dotenv";
 
-dotenv.config();
-// Create an axios instance with a base URL
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASEURL || "http://localhost:5000/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_BASEURL,
 });
 
-// Add a request interceptor to include the token in the headers
 api.interceptors.request.use((config) => {
   const token =
     typeof window !== "undefined" ? localStorage.getItem("token") : null;
